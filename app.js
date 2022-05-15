a1d5d87897283a3d0d5aae1de7b8ab0d03e27c00
RBzs <racz.balazs@yahoo.com>
2022-05-15 20:33:34 +0300

    Added npm, added package for reading input

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var msg = "Hello world";
-console.log(msg);
+const prompt = require('prompt-sync')({sigint: true}); //So we can exit with Ctrl+c
+
+/*
+console.log("Welcome to the Elevator Building");
+
+while (true) {
+    console.log("What would you like to do?");
+    console.log("Options:");
+    console.log("1. Exit");
+}
+*/
+
+const name = prompt("Enter your name: ");
+console.log("Hello, " + name);
